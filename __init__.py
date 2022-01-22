77a92deb218f1078e86046e0529ba0bc0bc342b4
Tristan Strathearn <hello+tristan@hugemenace.co>
2022-01-22 15:47:29 +1000

    feat: add __init__.py with addon information

---
+++ __init__.py
@@ -0,0 +1,11 @@
+bl_info = {
+    "name": "ND",
+    "author": "HugeMenace",
+    "version": (0, 0, 1),
+    "blender": (3, 0, 0),
+    "location": "HugeMance ND — N Panel",
+    "description": "Non-destructive operations, tools, and workflows.",
+    "warning": "",
+    "doc_url": "https://hugemenace.co",
+    "category": "Object"
+}

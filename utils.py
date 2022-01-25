b7100ec0fc6cc9758ecfeedc77736331ef64cac6
Tristan Strathearn <hello+tristan@hugemenace.co>
2022-01-25 12:10:54 +1000

    fix: ensure utils.set_3d_cursor handles the different possible cursor rotation modes

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import bpy
-from mathutils import Vector, Quaternion, Matrix
+from mathutils import Vector, Matrix
 
 
 def averaged_vector(vector_list, size=3):
@@ -24,7 +24,14 @@ def create_rotation_matrix_from_face(world_matrix, face):
     return rotation.transposed()
 
 
-def set_3d_cursor(location=Vector(), rotation=Quaternion()):
+def set_3d_cursor(location, rotation):
     cursor = bpy.context.scene.cursor
     cursor.location = location
-    cursor.rotation_quaternion = rotation
+
+    if cursor.rotation_mode == 'QUATERNION':
+        cursor.rotation_quaternion = rotation
+    elif cursor.rotation_mode == 'AXIS_ANGLE':
+        cursor.rotation_axis_angle = rotation.to_axis_angle()
+    else:
+        cursor.rotation_euler = rotation.to_euler(cursor.rotation_mode)
+

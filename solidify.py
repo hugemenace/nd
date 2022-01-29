5586b6981d7314710aa602fa191867730f73c913
Tristan Strathearn <hello+tristan@hugemenace.co>
2022-01-29 12:05:45 +1000

    fix: update solidify weighting parameter name

--- solidify.py
+++ solidify.py
@@ -144,7 +144,7 @@ def draw_text_callback(self):
 
     draw_property(
         self, 
-        "Offset: {}".format(['Negative', 'Neutral', 'Positive'][1 + self.weighting]),
+        "Weighting: {}".format(['Negative', 'Neutral', 'Positive'][1 + self.weighting]),
         "Alt (Negative, Neutral, Positive)",
         active=self.key_alt,
         alt_mode=False)

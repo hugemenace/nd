916ad8062782356b4c43eba8d6136bfc61248136
Tristan Strathearn <hello+tristan@hugemenace.co>
2022-02-07 09:16:51 +1000

    chore: move vertex_bevel into power mods category

--- power_mods/__init__.py
+++ power_mods/__init__.py
@@ -2,23 +2,27 @@ import importlib
 from . import screw
 from . import solidify
 from . import weighted_normal_bevel
+from . import vertex_bevel
 
 
 def reload():
     importlib.reload(screw)
     importlib.reload(solidify)
     importlib.reload(weighted_normal_bevel)
+    importlib.reload(vertex_bevel)
 
 
 def register():
     screw.register()
     solidify.register()
     weighted_normal_bevel.register()
+    vertex_bevel.register()
 
 
 def unregister():
     screw.unregister()
     solidify.unregister()
     weighted_normal_bevel.unregister()
+    vertex_bevel.unregister()
 
     
--- sketching/__init__.py
+++ sketching/__init__.py
@@ -2,27 +2,23 @@ import importlib
 from . import blank_sketch
 from . import view_align
 from . import geo_lift
-from . import vertex_bevel
 
 
 def reload():
     importlib.reload(blank_sketch)
     importlib.reload(view_align)
     importlib.reload(geo_lift)
-    importlib.reload(vertex_bevel)
 
 
 def register():
     blank_sketch.register()
     view_align.register()
     geo_lift.register()
-    vertex_bevel.register()
 
 
 def unregister():
     blank_sketch.unregister()
     view_align.unregister()
     geo_lift.unregister()
-    vertex_bevel.unregister()
 
     

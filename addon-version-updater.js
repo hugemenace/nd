a5fe138bc646e3952e98f54dab1887078f44b97a
Tristan Strathearn <hello+tristan@hugemenace.co>
2022-03-30 20:58:42 +1000

    chore: fix regex in addon-version-updater.js

--- addon-version-updater.js
+++ addon-version-updater.js
@@ -1,12 +1,12 @@
 module.exports.readVersion = function (contents) {
-    const regex = /"version": \(([0-9]), ([0-9]), ([0-9])\),/gm;
+    const regex = /"version": \(([0-9]+), ([0-9]+), ([0-9]+)\),/gm;
     const match = regex.exec(contents);
 
     return `${match[1]}.${match[2]}.${match[3]}`;
 }
 
 module.exports.writeVersion = function (contents, version) {
-    const regex = /"version": \([0-9], [0-9], [0-9]\),/gm;
+    const regex = /"version": \([0-9]+, [0-9]+, [0-9]+\),/gm;
     const v = version.split('.');
 
     return contents.replace(regex, `"version": (${v[0]}, ${v[1]}, ${v[2]}),`);

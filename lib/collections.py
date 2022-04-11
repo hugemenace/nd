c233fff3b8d890ad00ec29a61708848efe3688fa
Tristan Strathearn <hello+tristan@hugemenace.co>
2022-04-12 08:44:35 +1000

    chore: simplify lib/collections functions

--- lib/collections.py
+++ lib/collections.py
@@ -1,13 +1,6 @@
 import bpy
 from . preferences import get_preferences
 
-def has_utils_collection():
-    for collection in bpy.data.collections:
-        if collection.name == get_preferences().utils_collection_name:
-            return (True, collection)
-    
-    return (False, None)
-        
 
 def create_utils_collection():
     collection = bpy.data.collections.new(get_preferences().utils_collection_name)
@@ -19,12 +12,12 @@ def create_utils_collection():
 
 
 def move_to_utils_collection(obj):
-    (has_utils, utils_collection) = has_utils_collection()
-    if not has_utils:
-        utils_collection = create_utils_collection()
+    collection = bpy.data.collections.get(get_preferences().utils_collection_name)
+    if collection is None:
+        collection = create_utils_collection()
     
     remove_obj_from_all_collections(obj)
-    utils_collection.objects.link(obj)
+    collection.objects.link(obj)
 
 
 def remove_obj_from_all_collections(obj):

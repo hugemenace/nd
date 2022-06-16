a14b8194156b4b8475ad21d5106202a9b99f6fdd
Tristan Strathearn <hello+tristan@hugemenace.co>
2022-06-16 12:08:25 +1000

    feat: add solidify option to fast menu for non-manifold meshes

--- interface/fast_menu.py
+++ interface/fast_menu.py
@@ -153,6 +153,7 @@ class ND_MT_fast_menu(bpy.types.Menu):
             self.sketch = len(self.faces) >= 1 and is_planar(bm)
             self.profile = len(self.faces) == 0 and len(self.edges) > 0
             self.form = len(self.faces) > 1
+            self.manifold = all([len(edge.link_faces) == 2 for edge in self.edges])
 
             bm.free()
 
@@ -181,7 +182,7 @@ class ND_MT_fast_menu(bpy.types.Menu):
                 layout.operator("nd.cycle", icon=icons['nd.cycle'])
                 layout.separator()
 
-            if has_mod_solidify:
+            if not self.manifold or has_mod_solidify:
                 layout.operator("nd.solidify", icon=icons['nd.solidify'])
 
             if has_mod_profile_extrude:

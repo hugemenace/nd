bb0fa1191ae7d1ede1b514a0426590d8d5acb7ea
Tristan Strathearn <hello+tristan@hugemenace.co>
2022-07-23 14:11:27 +1000

    feat: add curve support to simple_deform operator

--- deform/simple_deform.py
+++ deform/simple_deform.py
@@ -174,7 +174,7 @@ class ND_OT_simple_deform(bpy.types.Operator):
     @classmethod
     def poll(cls, context):
         if context.mode == 'OBJECT':
-            return len(context.selected_objects) == 1 and context.active_object.type == 'MESH'
+            return len(context.selected_objects) == 1 and context.active_object.type in ['MESH', 'CURVE']
 
     
     def prepare_new_operator(self, context):

6110667ac91e9048f8d2baa42a7e98641b13db7c
Tristan Strathearn <hello+tristan@hugemenace.co>
2024-03-22 11:26:38 +1100

    chore: wip

--- lib/events.py
+++ lib/events.py
@@ -63,9 +63,9 @@ def capture_modifier_keys(cls, event=None, mouse_x=0):
     cls.key_step_down = detected(event, {'WHEELDOWNMOUSE'}) or pressed(event, {'DOWN_ARROW'}) or pressed(event, {'LEFT_ARROW'})
     
     if get_preferences().enable_right_click_select:
-        cls.key_confirm = clicked(event, {'RIGHTMOUSE'}) or pressed(event, {'SPACE', 'RET', 'NUMPAD_ENTER'})
+        cls.key_confirm = clicked(event, {'LEFTMOUSE'}) or pressed(event, {'SPACE', 'RET', 'NUMPAD_ENTER'})
         cls.key_select = detected(event, {'RIGHTMOUSE'})
-        cls.key_cancel = clicked(event, {'LEFTMOUSE'}) or pressed(event, {'ESC'})
+        cls.key_cancel = pressed(event, {'ESC'})
     else:
         cls.key_confirm = clicked(event, {'LEFTMOUSE'}) or pressed(event, {'SPACE', 'RET', 'NUMPAD_ENTER'})
         cls.key_select = detected(event, {'LEFTMOUSE'})

56b5b6c7d145ed41a50f09c2701b093c16dc50df
Tristan Strathearn <hello+tristan@hugemenace.co>
2024-03-22 11:07:48 +1100

    chore: add a version updater for the manifest file

---
+++ manifest-version-updater.js
@@ -0,0 +1,40 @@
+// ███╗   ██╗██████╗ 
+// ████╗  ██║██╔══██╗
+// ██╔██╗ ██║██║  ██║
+// ██║╚██╗██║██║  ██║
+// ██║ ╚████║██████╔╝
+// ╚═╝  ╚═══╝╚═════╝ 
+// 
+// ND (Non-Destructive) Blender Add-on
+// Copyright (C) 2024 Tristan S. & Ian J. (HugeMenace)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// ---
+// Contributors: Tristo (HM)
+// ---
+
+module.exports.readVersion = function (contents) {
+    const regex = /version = "([0-9]+)\.([0-9]+)\.([0-9]+)"/gm;
+    const match = regex.exec(contents);
+
+    return `${match[1]}.${match[2]}.${match[3]}`;
+}
+
+module.exports.writeVersion = function (contents, version) {
+    const regex = /version = "[0-9]+\.[0-9]+\.[0-9]+"/gm;
+    const v = version.split('.');
+
+    return contents.replace(regex, `version = "${v[0]}.${v[1]}.${v[2]}"`);
+}

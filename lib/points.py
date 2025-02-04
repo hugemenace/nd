665132fff438a86bf430781ceb1a3395cb3f6670
Tristan Strathearn <hello+tristan@hugemenace.co>
2025-02-04 15:22:28 +1100

    chore: add disc option to lib/points

--- lib/points.py
+++ lib/points.py
@@ -34,7 +34,7 @@ from . preferences import get_preferences
 from . polling import app_minor_version
 
 
-vertex_shader = '''
+generic_vertex_shader = '''
     uniform mat4 viewProjectionMatrix;
     in vec3 pos;
 
@@ -43,19 +43,38 @@ vertex_shader = '''
     }
 '''
 
-fragment_shader = '''
+circle_fragment_shader = '''
     uniform vec4 color;
     out vec4 fragColor;
 
-    float circle(in vec2 origin, in float radius) {
-        vec2 dist = origin - vec2(0.5);
-        return 1.0 - smoothstep(radius - (radius * 0.35), radius + (radius * 0.01), dot(dist, dist) * 4.0);
+    float circle(in vec2 st, in float radius, in float smoothing) {
+        float perc = distance(st, vec2(0.5));
+        return 1.0 - smoothstep(radius, radius + smoothing, perc);
     }
 
     void main() {
-        vec4 circle = vec4(circle(gl_PointCoord, 0.5));
+        vec4 c = vec4(circle(gl_PointCoord, 0.4, 0.1));
         float gamma = 2.2;
-        fragColor = pow(circle * color, vec4(gamma));
+        fragColor = pow(c * color, vec4(gamma));
+    }
+'''
+
+disc_fragment_shader = '''
+    uniform vec4 color;
+    out vec4 fragColor;
+
+    float disc(in vec2 st, in float radius, in float thickness, in float smoothing) {
+        float perc = distance(st, vec2(0.5));
+        float outline = 1.0 - smoothstep(radius, radius + smoothing, perc);
+        float center = smoothstep(radius - thickness, (radius - thickness) + smoothing, perc);
+
+        return outline * center;
+    }
+
+    void main() {
+        vec4 d = vec4(disc(gl_PointCoord, 0.4, 0.018, 0.02));
+        float gamma = 2.2;
+        fragColor = pow(d * color, vec4(gamma));
     }
 '''
 
@@ -91,9 +110,21 @@ def redraw_regions():
 
 def init_points(cls):
     cls.primary_points = []
+    cls.disc_points = []
     cls.secondary_points = []
     cls.tertiary_points = []
     cls.guide_line = ()
+
+
+def draw_discs(shader, points, radius, color):
+    gpu.state.point_size_set(radius)
+    batch = batch_for_shader(shader, 'POINTS', {"pos": points})
+    gpu.state.blend_set("ALPHA")
+    shader.bind()
+    view_projection_matrix = bpy.context.region_data.perspective_matrix
+    shader.uniform_float("viewProjectionMatrix", Matrix(view_projection_matrix))
+    shader.uniform_float("color", color)
+    batch.draw(shader)
 
 
 def draw_circles(shader, points, radius, color):
@@ -119,7 +150,8 @@ def draw_guideline(shader, line, size, color):
 
 def update_points(cls):
     shader = None
-    circle_shader = gpu.types.GPUShader(vertex_shader, fragment_shader)
+    circle_shader = gpu.types.GPUShader(generic_vertex_shader, circle_fragment_shader)
+    disc_shader = gpu.types.GPUShader(generic_vertex_shader, disc_fragment_shader)
 
     if app_minor_version() < (4, 0):
         shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
@@ -130,5 +162,6 @@ def update_points(cls):
     draw_circles(circle_shader, cls.primary_points, 20, get_preferences().points_primary_color)
     draw_circles(circle_shader, cls.secondary_points, 15, get_preferences().points_secondary_color)
     draw_circles(circle_shader, cls.tertiary_points, 25, get_preferences().points_tertiary_color)
+    draw_discs(disc_shader, cls.disc_points, 200, get_preferences().points_primary_color)
 
     redraw_regions()
